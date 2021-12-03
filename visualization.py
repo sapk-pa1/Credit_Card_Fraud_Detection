7d7e0773b99aabc3be22606f9fc148594325482d
pa1 <sapkotapawan099@gmail.com>
2021-12-03 15:48:22 +0545

    Changes

---
+++ visualization.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import warnings 
+
+warnings.filterwarnings('ignore')
+#ignoreing all the futture warnings 
+
+def visualize_data(data_needed_for_viz):
+    sample_data, valid, fraud, v_features = data_needed_for_viz
+    #bar plot for the valid and the fraud cases 
+    x = ["Valid", "Fraud"]
+    y = [valid.size, fraud.size]
+    fig, ax = plt.subplots()
+    plt.yticks([0,500000,1000000,4000000,5000000])
+    ax.bar(x, y, color = ('green', 'red'))
+    ax.set(title= "Credit Card Transaction Types", ylabel = "No of Transactions")
+
+    #Text rakheko ho tyo plot ko top ma 
+    for i in range(len(x)):
+        plt.text(i,y[i],str(y[i]), ha= "center", va= 'bottom', color ='black' )
+    plt.show()
+    fig, ax = plt.subplots(1, 2, figsize=(18,4))
