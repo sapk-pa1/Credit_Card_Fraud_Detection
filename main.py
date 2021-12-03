844a08cc6d75b7ad6856f8429e50d68d71bccd74
pa1 <sapkotapawan099@gmail.com>
2021-12-03 20:13:54 +0545

    SMOTE

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 from visualization import visualize_data 
+from smote import oversampling
+from sklearn.model_selection import train_test_split
 
 
 ## main file for the project 
@@ -7,7 +9,9 @@ from visualization import visualize_data
 
 dataset = pd.read_csv("./dataset/creditcard.csv")
 sample_data = sample_data = dataset.sample(frac = 0.5, random_state = 1)
-
+x = sample_data.drop(['Class', 'Time'], axis = 1)
+y = sample_data['Class']
+x_train , x_test , y_train, y_test = train_test_split(x,y,test_size= 0.6, random_state=12)
 # Fraud and Valid case from the sample data 
 fraud= sample_data[sample_data["Class"] ==1]
 valid = sample_data[sample_data["Class"]==0]
@@ -17,3 +21,4 @@ v_features = ["V1","V2","V3","V4","V5","V6","V7","V8","V9","V10","V11","V12","V1
 "V14","V15","V16","V17","V18","V19","V20","V21","V22","V23","V24","V25","V26","V27","V28"]
 data_needed_for_visualization = [sample_data, valid, fraud, v_features]
 visualize_data(data_needed_for_visualization)
+oversampling(x_train, y_train)
--- smote.py
+++ smote.py
@@ -1,19 +1,13 @@
-from sklearn.model_selection import train_test_split
-x_train , x_test , y_train, y_test = train_test_split(x,y,test_size= 0.6, random_state=12)
 
 from imblearn.over_sampling import SMOTE
 
 
-oversample = SMOTE()
-
-overSM_x_train,overSM_y_train = oversample.fit_resample(x_train,y_train)
-
-
-
-
-##After Oversampling using the smote 
-print("After Oversampling:")
-no_of_fraud_records = len(overSM_y_train[overSM_y_train==1])
-print(no_of_fraud_records)
-no_of_valid_records = len(overSM_y_train[overSM_y_train==0])
-print(no_of_valid_records)
+def oversampling(x_train, y_train):
+    oversample = SMOTE()
+    overSM_x_train,overSM_y_train = oversample.fit_resample(x_train,y_train)
+    ##After Oversampling using the smote 
+    print("After Oversampling:")
+    no_of_fraud_records = len(overSM_y_train[overSM_y_train==1])
+    print(no_of_fraud_records)
+    no_of_valid_records = len(overSM_y_train[overSM_y_train==0])
+    print(no_of_valid_records)
